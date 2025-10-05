1a80078bd9172c295f0bd6b9eccc8bf9290927f6
siwoo2013 <82808783+siwoo2013@users.noreply.github.com>
2025-10-05 09:04:43 +0900

    Delete src/components/NavBar.jsx

--- src/components/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-export default function NavBar() {
-  return (
-    <nav className="nav">
-      {['Home', 'Event', 'Invest', 'Service', 'MY'].map((tab) => (
-        <button key={tab}>{tab}</button>
-      ))}
-    </nav>
-  )
-}
